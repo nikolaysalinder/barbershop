c2833c81ba31680c2276c98d0198404453eb29c6
nikolaysalinder <nikolay.salinder@gmail.com>
2018-02-03 15:39:02 +0500

    delet '../' from gallery and index-logo for gh-pages

--- src/components/IndexLogo.vue
+++ src/components/IndexLogo.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="index-logo clearfix">
-      <img src="../assets/img/index-logo.png" :alt="alt" >
+      <img src="assets/img/index-logo.png" :alt="alt" >
     </div>
 </template>
 

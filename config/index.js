08028b1e2f463a33735b86c863654659d14a38f2
nikolaysalinder <nikolay.salinder@gmail.com>
2018-02-07 06:44:54 +0500

    change 'build/config.js' file assetsSubDirecotry to barbershop/static

--- config/index.js
+++ config/index.js
@@ -49,8 +49,8 @@ module.exports = {
 
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
-    assetsSubDirectory: 'static',
-    assetsPublicPath: '/barbershop/',
+    assetsSubDirectory: 'barbershop/static',
+    assetsPublicPath: '/',
 
     /**
      * Source Maps

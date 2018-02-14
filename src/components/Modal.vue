b75ccc464f0ca99a60f6da9009733fd6437e0dee
nikolaysalinder <nikolay.salinder@gmail.com>
2018-02-14 12:33:00 +0500

    Added overflow to overlay in modal

--- src/components/Modal.vue
+++ src/components/Modal.vue
@@ -46,17 +46,11 @@ export default {
   position: fixed;
   top: 0;
   left: 0;
+  right: 0;
+  bottom: 0;
   background-color: rgba(0,0,0,.3);
-  width: 100%;
-  height: 100%;
   z-index: 10;
-}
-.modal-overlay:after {
-  display: block;
-  width:  100%;
-  height: 10px;
-  vertical-align: middle;
-  content: '';
+  overflow: auto;
 }
 .modal {
   position: fixed;
